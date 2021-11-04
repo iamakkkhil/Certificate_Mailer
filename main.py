ba3728d7dfb003f5bf9777e69bd1c735a5a1d086
Akhil Bhalerao <iamakhil1408@gmail.com>
2021-11-04 13:58:18 +0530

    mail sender ready

--- main.py
+++ main.py
@@ -1,42 +1,130 @@
-from pptx import Presentation 
+from pptx import Presentation
 from pptx.util import Pt
 import os
-import comtypes.client
+import pandas as pd
 
-# Opening file
-prs = Presentation('assets/Both_Tracks_Golden_Certificate.pptx')
-slide = prs.slides[0]
-name = "Akhil Bhalerao"
+# import comtypes.client
+import smtplib, ssl
 
-for shape in slide.shapes:
-    if shape.has_text_frame:
-        text_frame = shape.text_frame
-        
-        if (text_frame.text == 'Name'):
-            text_frame.clear()
-            p = text_frame.paragraphs[0]
-            run = p.add_run()
-            run.text = name
-            font = run.font
-            font.name = 'Caveat'
-            font.size = Pt(40)
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-prs.save(f"output/Output_{name}.pptx")
+from dotenv import load_dotenv
 
-input_file_path = os.path.abspath(f"output/Output_{name}.pptx")
-output_file_path = os.path.abspath(f"output/Output_{name}.pdf")
+load_dotenv()
 
-#%% Create powerpoint application object
-powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+def delete_pptx(names):
+    for name in names:
+        try:
+            os.remove(f"output/Output_{name}.pptx")
+        except Exception as e:
+            pass
 
-#%% Set visibility to minimize
-powerpoint.Visible = 1
 
-#%% Open the powerpoint slides
-slides = powerpoint.Presentations.Open(input_file_path)
+def read_csv(filename):
+    df = pd.read_csv(filename)
+    emails = df.iloc[:, 1]
+    names = df.iloc[:, 0]
+    return names, emails
 
-#%% Save as PDF (formatType = 32)
-slides.SaveAs(output_file_path, 32)
 
-#%% Close the slide deck
-slides.Close()
+def add_name_to_ppt(ppt_file_path, names):
+    # Opening file
+    for name in names:
+        prs = Presentation(ppt_file_path)
+        slide = prs.slides[0]
+
+    # Prinitng everyones name on the certificate
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                text_frame = shape.text_frame
+
+                if text_frame.text == "Name":
+                    text_frame.clear()
+                    p = text_frame.paragraphs[0]
+                    run = p.add_run()
+                    run.text = name
+                    font = run.font
+                    font.name = "Caveat"
+                    font.size = Pt(35)
+
+        prs.save(f"output/Output_{name}.pptx")
+
+
+def ppt_to_pdf(input_file_path, output_file_path):
+    # Create powerpoint application object
+    powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+    # Set visibility to minimize
+    powerpoint.Visible = 1
+    # Open the powerpoint slides
+    slides = powerpoint.Presentations.Open(input_file_path)
+    # Save as PDF (formatType = 32)
+    slides.SaveAs(output_file_path, 32)
+    # Close the slide deck
+    slides.Close()
+
+
+def send_mail(file_path, user_name):
+    subject = "An email with attachment from Python"
+    body = "This is an email with attachment sent from Python"
+    sender_email = os.environ.get("EMAIL")
+    receiver_email = "akhilbhalerao@gmail.com"
+    password = os.environ.get("PASSWORD")
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+
+    filename = file_path  # In same directory as script
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {user_name} Certificate.pptx",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        print("Logging In...")
+        server.login(sender_email, password)
+        print("Logged In...")
+        print("Sending mail...")
+        server.sendmail(sender_email, receiver_email, text)
+        print(f"Mail sent successfully to {user_name}.")
+
+
+if __name__ == "__main__":
+    user_name = "Akhil Bhalerao"
+    ppt_file_path = "assets/Both_tracks.pptx"
+    input_file_path = os.path.abspath(f"output/Output_{user_name}.pptx")
+    output_file_path = os.path.abspath(f"output/Output_{user_name}.pptx")
+    user_data_file_path = os.path.abspath(
+        "User_details/Both_Track_Winners_Data.csv"
+    )
+    # send_mail(output_file_path, user_name)
+    names, emails =read_csv(user_data_file_path)
+    add_name_to_ppt(ppt_file_path, names)
+    # delete_pptx(names)
+    
